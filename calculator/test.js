8a9ce9944093a2accac5a080a3c945ebec384dcc
cshan <cshan@forcs.com>
2021-05-04 03:52:20 +0900

    계산기 자바스크립트 업로드

---
+++ calculator/test.js
@@ -0,0 +1,33 @@
+function test(){
+  let result = '';
+
+  if(a) {
+    if(!b) {
+      result = 'c';
+    }
+  } else {
+    result = 'a';
+  }
+  result = 'b';
+  return result;
+}
+
+
+
+
+function test() {
+  let result = '';
+   
+  if(!a) {
+    result = 'a';
+    result += 'b';
+    return result;
+  } 
+     
+  if(!b) {
+    result = 'c';
+  }
+  
+  result += 'b';
+  return result;
+}
