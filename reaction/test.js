e0e42cf4719d08a68158c43898380b409c7b19a9
hcs1105 <hcs1105/gmail/com>
2021-05-23 20:54:51 +0900

    1. 화면을 클릭할 때 걸리게 될 처음 시간과 마지막 시간을 구하는 로직 구현 2. 자바스크립트로 클래스를 추가/수정/변경하는 문제 풀이 추가 3. 자바스크립트로 시간 차를 구현하는 문제 풀이 추가

--- reaction/test.js
+++ reaction/test.js
@@ -4,3 +4,19 @@
  * 태그.classList.replace('기존 클래스', '수정 클래스'); // 수정
  * 태그.classList.remove('클래스'); // 삭제
  */
+
+// 다음 태그의 클래스는 어떻게 될까요?
+태그.classList.add('hello', 'hi', 'hello', 'bye'); // 'hello', 'hi', 'bye'
+태그.classList.replace('bye', 'seeyou'); // 'hello', 'hi', 'seeyou'
+태그.classList.remove('bye'); // 'hello', 'hi', 'seeyou'
+
+// 2021년 2월 21일과 2021년 3월 3일은 며칠 차이가 날끼요?
+/*
+ * new Date(2021, 2, 3) - new Date(2021, 1, 21) // 864000000ms
+ * (new Date(2021, 2, 3) - new Date(2021, 1, 21)) / 1000 // 864000s
+ * (new Date(2021, 2, 3) - new Date(2021, 1, 21)) / 1000 / 60 // 14400m
+ * (new Date(2021, 2, 3) - new Date(2021, 1, 21)) / 1000 / 60 / 60 // 240h
+ * (new Date(2021, 2, 3) - new Date(2021, 1, 21)) / 1000 / 60 / 60 / 24 // 10days
+ */
+const differDays = new Date(2021, 2, 3) - new Date(2021, 1, 21);
+console.log(differDays / 1000 / 60 / 60 / 24); // 10
