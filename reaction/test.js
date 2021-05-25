5571d49c879d3cf68a3630526060c5a34db62f88
hcs1105 <hcs1105@gmail.com>
2021-05-26 02:29:23 +0900

    1. 성급하게 클릭하지 않도록 로직 추가 2. 예시를 통해  Date 객체의 내용 정리 3. reduce() 메소드의 내용 보완

--- reaction/test.js
+++ reaction/test.js
@@ -10,7 +10,20 @@
 태그.classList.replace('bye', 'seeyou'); // 'hello', 'hi', 'seeyou'
 태그.classList.remove('bye'); // 'hello', 'hi', 'seeyou'
 
-// 문재 2. 2021년 2월 21일과 2021년 3월 3일은 며칠 차이가 날끼요?
+/*
+ * new Date(); // Wed May 26 2021 01:56:24 GMT+0900 (대한민국 표준시)
+ * new Date().getFullYear(); // 2021
+ * new Date().getMonth(); // 4
+ * new Date().getDate(); // 26
+ * new Date().getHours(); // 2
+ * new Date().getMinutes(); // 3
+ * new Date().getSeconds(); // 10
+ * new Date().getMilliseconds(); // 865
+ * new Date().setDate(23); // 1621703198436
+ * new Date().toLocaleString(); // "2021. 5. 26. 오전 2:08:22"
+ */
+
+// 문제 2. 2021년 2월 21일과 2021년 3월 3일은 며칠 차이가 날끼요?
 /*
  * new Date(2021, 2, 3) - new Date(2021, 1, 21) // 864000000ms
  * (new Date(2021, 2, 3) - new Date(2021, 1, 21)) / 1000 // 864000s
@@ -23,6 +36,11 @@ console.log(differDays / 1000 / 60 / 60 / 24); // 10
 
 /* 
  * reduce() 메소드의 사용 방법
+ * 배열.reduce((누적값, 현재값) => {
+ *  return 새로운 누적값;
+ * }, 초기값);
+ * 함수에서 반환돠는 값은 다음 번에 누적값이 되고 마지막으로 반환되는 값은 reduce 메소드의 결과값임.
+ * 초기값이 없으면 배열의 첫 번째 요소가 초기값임.
  * reduce() 메소드는 배열과 알고리즘에서 많이 쓰임.
  * reduce() 메소드를 이용하여 배열을 객체 리터럴로 바꿀 수 있다.
  * reduce() 메소드를 이용하여 forEach() 문과 map() 메소드, find() 메소드, findIndex() 메소드, include() 메소드, indexOf() 메소드 등을 구현할 수 있다.
