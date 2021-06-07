6fbd1feafbdfbc4bf63e9b81fdd8d094f6497603
hcs1105 <hcs1105@gmail.com>
2021-06-07 23:41:47 +0900

    1. JSON.parse() 메소드와 JSON.stringfy() 메소드 내용 정리 2. 깊은 복사와 얕은 복사의 개념 업데이트

---
+++ textRpg/test.js
@@ -0,0 +1,17 @@
+/*
+ * 문제 1. 다음 다섯 개의 값을 각각 복사해 보세요. 여기서 복사라고 함은 복사본을 수정할 때 원본이 바뀌지 않는 것을 
+ * 의미합니다. 객체라면 복사한 객체 내부의 값을 바꿔도 원본 객체의 값이 바뀌지 않아야 합니다.
+ */
+
+const a = 'b';
+const c = ['d', true, 1];
+const e = {g : 'h'};
+const i = [{ j : 'k'}, { l : 'm'}];
+const n = { o : { p : 'q'}};
+
+const a1 = a;
+const c1 = c.slice(); // 변수 C의 배열 안에 있는 요소들이 원시값이므로 slice() 메소드를 사용해도 됨.
+const c1 = [...c];
+const e1 = {...e}; // 객체 안에 객체가 들어 있지 않으므로
+const i1 = JSON.parse(JSON.stringify(i));
+const n1 = JSON.parse(JSON.stringify(n));
