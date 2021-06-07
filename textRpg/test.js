4ae8a9bff2102d48886a2dfa7914ed635ebfdd61
hcs1105 <hcs1105@gmail.com>
2021-06-08 00:20:45 +0900

    1. 주인공과 괴물이 서로 공격하는 로직 구현 중 2. concat() 메소드로 복사를 구현하는 로직 구현

--- textRpg/test.js
+++ textRpg/test.js
@@ -15,3 +15,12 @@ const c1 = [...c];
 const e1 = {...e}; // 객체 안에 객체가 들어 있지 않으므로
 const i1 = JSON.parse(JSON.stringify(i));
 const n1 = JSON.parse(JSON.stringify(n));
+
+/*
+ * 유제 1. 다음 값을 concat 메소드로 복사해 보세요. 여기서 복사라고 함은 복사본을 수정할 때 원본이 바뀌지 않는 것을 
+ * 의미합니다. 객체라면 복사한 객체 내부의 값을 바꿔도 원본 객체의 값이 바뀌지 않아야 합니다.
+ */
+
+const g = ['1'];
+
+const g1 = g.concat(); // ["1"]  
