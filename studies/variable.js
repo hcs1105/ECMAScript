b44592f01a28d8c7a2875fdac0820577dfb9a7d1
hcs1105 <hcs1105@gmail.com>
2023-02-14 19:52:20 +0900

    variable types 내용 정리

--- studies/variable.js
+++ studies/variable.js
@@ -39,3 +39,71 @@ console.log(age); // 7
 
 const dayInWeek = 7;
 const maxNumber = 5;
+
+// 4. Variable types
+// primitive, sing item - number, string, boolean, undefined, null, symbol 
+// object - box container
+// function - first-class function
+
+let a = 12; // TS -> let a : number = 12;
+let b = 1.2; // TS -> let b : number = 1.2;
+
+const count = 17; // integer
+const size = 17.1; // decimal number
+
+console.log(`value: ${count}, type: ${typeof count}`); // value: 17, type: number
+console.log(`value: ${size}, type: ${typeof size}`); // value: 17.1, type: number
+
+// number - special numberic values : infinity, -infinity, NaN
+const infinity = 1 / 0;
+const negativeInfinity = -1 / 0;
+const nAn = 'not a number' / 2;
+
+console.log(infinity); // Infinity
+console.log(negativeInfinity); // -Infinity
+console.log(nAn); // NaN
+
+// bigInt(fairly new, don't use it yet)
+// support browsers : chrome, firefox, safari(2020년 4월 11일에는 지원되지 않았지만, 2023년 2월 14일 현재 지원됨)
+const bigInt = 1234567890123456789012345678901234567890n; // over(-2**53 ~ 2**53)
+console.log(`value: ${bigInt}, type: ${typeof bigInt}`); // value: 1234567890123456789012345678901234567890, type: bigint
+Number.MAX_SAFE_INTEGER;
+
+// string
+const char  = 'c';
+const jsCreator = 'brendan';
+const greeting = 'hello ' + jsCreator;
+
+console.log(`value: ${greeting}, type: ${typeof greeting}`); // value: hello brendan, type: string
+const helloBob = `hi ${jsCreator}!`; // templete literals(string)
+console.log(`value: ${helloBob}, type: ${typeof helloBob}`); // value: hi brendan!, type: string
+// 같은 표현 : console.log('value: ' + helloBob + ', ' + 'type: ' + typeof helloBob);
+
+// boolean
+// true : any other value
+// false : 0, null, undefined, NaN, ''
+
+const canRead = true;
+const test = 3 < 1; // false;
+
+console.log(`value: ${canRead}, type: ${typeof canRead}`); // value: true, type: boolean
+console.log(`value: ${test}, type: ${typeof test}`); // value: false, type: boolean
+
+// null
+let nothing = null;
+console.log(`value: ${nothing}, type: ${typeof nothing}`); // value: null, type: object
+
+// undefined
+let x;
+console.log(`value: ${x}, type: ${typeof x}`); // value: undefined, type: undefined
+
+// symbol, create unique identifiers for objects
+const symbol1 = Symbol('id');
+const symbol2 = Symbol('id');
+const symbol3 = Symbol.for('id');
+const symbol4 = Symbol.for('id');
+
+console.log(symbol1 === symbol2); // false
+console.log(symbol3 === symbol4); // true
+// console.log(`value: ${symbol1}, type: ${typeof symbol1}`); // Uncaught TypeError: Cannot convert a Symbol value to a string
+console.log(`value: ${symbol1.description}, type: ${typeof symbol1}`); // value: id, type: symbol
