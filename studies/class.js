86fd7c7ea4dcc835825fdb1e4d38025c6c3b206f
hcs1105 <hcs1105@gmail.com>
2023-02-22 20:02:21 +0900

    클래스와 오브젝트의 차이점 정리

---
+++ studies/class.js
@@ -0,0 +1,23 @@
+/*
+class person {
+  name; // 속성(field)
+  age; // 속성(field)
+  speak(); // 행동(method)
+}
+*/
+
+/** 
+ * class
+ * - template
+ * - declare once
+ * - no data in
+ */
+
+/** 
+ * object
+ * - instance of a class
+ * - created many times
+ * - data in
+ */
+
+'use strict';
