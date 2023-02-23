7f19724409c3da438c4d724a9e128c4168cfe683
hcs1105 <hcs1105@gmail.com>
2023-02-23 14:52:41 +0900

    Object 개념 정리 중

---
+++ studies/object.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// 참고 URL - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object
+
+// Object
+// one of the JavaScript's data types.
+// A collection of related data and/or functionality.
+// Nearly all objects in JavaScript area instances of Object.
+
+const name = 'hcs1105';
+const age = 20;
+
+print(name, age);
+
+function print(name, age) {
+	console.log(name); // hcs1105
+	console.log(age); // 20
+}
+
+const hcs1105 = {
+	name : 'hcs1105',
+	age : 20
+}
