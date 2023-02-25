d5a7df1a6db03af77c440659b11b9e15b70e35b5
hcs1105 <hcs1105@gmail.com>
2023-02-25 19:00:07 +0900

    배열 혹은 원소를 삽입•삭제하는 방법 설명

--- studies/array.js
+++ studies/array.js
@@ -42,3 +42,43 @@ fruits.forEach((fruit) => {
 
 // 화살표 함수를 간단히 사용하는 방법
 fruits.forEach(fruit => console.log(fruit)); // 사과, 바나나
+
+// 4. Addition, Deletion
+// push : add an item to the end
+fruits.push('딸기', '복숭아');
+console.log(fruits); // ['사과', '바나나', '딸기', '복숭아']
+
+// pop : remove an item from the end
+fruits.pop();
+console.log(fruits); // ['사과', '바나나', '딸기']
+fruits.pop();
+console.log(fruits); // ['사과', '바나나']
+
+// unshift : add an item to the beginning
+fruits.unshift('딸기', '레몬');
+console.log(fruits); // ['딸기', '레몬', '사과', '바나나']
+
+// shift : remove an item from the beginning
+fruits.shift();
+console.log(fruits); // ['레몬', '사과', '바나나']
+fruits.shift();
+console.log(fruits); // ['사과', '바나나']
+
+// note!! shift and unshift are slower than push and pop.
+
+// splice : remove an item by index position
+fruits.push('딸기', '복숭아', '레몬');
+console.log(fruits); // ['사과', '바나나', '딸기', '복숭아', '레몬']
+/*
+fruits.splice(1);
+console.log(fruits); // ['사과']
+*/
+fruits.splice(1, 1);
+console.log(fruits); // ['사과', '딸기', '복숭아', '레몬']
+fruits.splice(1, 1, '배', '수박');
+console.log(fruits); // ['사과', '배', '수박', '복숭아', '레몬']
+
+// combine two arrays
+const fruit2 = ['포도', '자두'];
+const newFruits = fruits.concat(fruit2);
+console.log(newFruits); // ['사과', '배', '수박', '복숭아', '레몬', '포도', '자두']
