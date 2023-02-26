0da5bfad6edbbcb8bbab2b1af11f5485d63b8cb9
hcs1105 <hcs1105@gmail.com>
2023-02-26 19:56:51 +0900

    find() 메소드 내용 정리

--- studies/arrayQuiz.js
+++ studies/arrayQuiz.js
@@ -42,3 +42,34 @@
   console.log(array); // [3, 4, 5]
   */
 }
+
+class Student {
+  constructor(name, age, enrolled, score) {
+    this.name = name;
+    this.age = age;
+    this.enrolled = enrolled;
+    this.score = score;
+  }
+}
+
+const students = [
+  new Student('A', 29, true, 45),
+  new Student('B', 28, false, 80),
+  new Student('C', 30, true, 90),
+  new Student('D', 40, false, 66),
+  new Student('E', 18, true, 88)
+]
+
+// 5. find a student with the score 90.
+{
+  const result = students.find((student, index) => student.score === 90);
+  console.log(result); // Student {name: 'C', age: 30, enrolled: true, score: 90}
+
+  /* 같은 표현 : 함수 선언문 형식
+  const result = students.find(function(student, index){
+    // console.log(student, index);
+    return student.score === 90;
+  });
+  console.log(result); // Student {name: 'C', age: 30, enrolled: true, score: 90}
+  */
+}
