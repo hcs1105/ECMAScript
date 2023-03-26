e24180212a82d8128600bc70166701c49ed60f0d
hcs1105 <hcs1105@gmail.com>
2023-03-26 18:24:03 +0900

    동기적 혹은 비동기적으로 콜백이 발생하는 사례 추가

--- studies/basic/async/callback.js
+++ studies/basic/async/callback.js
@@ -7,8 +7,23 @@ console.log(1); // 1
 console.log(2); // 2
 console.log(3); // 3
 
-// asynchronous
+// Asynchronous
 // 콘솔 메소드에 찍힌 순서 : 1 -> 3 -> 2
 console.log(1);
 setTimeout(() => console.log(2), 1000);
 console.log(3);
+
+// Synchronous callback 
+function printImmediately(print) {
+  console.log(print); // () => console.log('hello')
+  print(); // hello
+}
+
+printImmediately(() => console.log('hello'));
+
+// Asynchronous callback
+function printWithDelay(print, time) {
+  setTimeout(print, time);
+}
+
+printWithDelay(() => console.log('Asynchronous callback!'), 2000); // Asynchronous callback!
