38e027d9de62fb869c53fd76c04cd2cbcf900f34
hcs1105 <hcs1105@gmail.com>
2023-03-26 16:01:46 +0900

    JSON 내용 추가 정리 완료

--- studies/basic/json.js
+++ studies/basic/json.js
@@ -17,42 +17,64 @@ console.log(jsonType); // string
 
 json = JSON.stringify(['apple', 'banner']);
 console.log(json); // ["apple","banner"]
+console.log(jsonType); // string
 
 const rabbit = {
   name : 'hcs1105',
   color : 'white',
   size : null,
   birthDate : new Date(),
-  // Symbol : Symbol('id'), 
-  jump : () => {
+  // Symbol : Symbol('id'), -> JSON에 포함되지 않음.
+  jump : () => { // 함수는 오브젝트의 데이터가 아니므로 JSON에 포함되지 않음.
     console.log(`${this.name} can be jump!`);
   }
 };
 
 json = JSON.stringify(rabbit);
 console.log(json); // {"name":"hcs1105","color":"white","size":null,"birthDate":"2023-02-28T07:21:46.096Z"}
+console.log(jsonType); // string
 
-json = JSON.stringify(rabbit, ['name']);
+json = JSON.stringify(rabbit, ['name']); // 파라미터 replacer를 사용한 경우
 console.log(json); // {"name":"hcs1105"}
+console.log(jsonType); // string
 
 json = JSON.stringify(rabbit, ['name', 'color']);
 console.log(json); // {"name":"hcs1105","color":"white"}
+console.log(jsonType); // string
 
+// 콜백 함수를 이용하영 파라미터 replacer를 표현한다면
 json = JSON.stringify(rabbit, (key, value) => {
   console.log(`key : ${key}, value : ${value}`);
   return key === 'name' ? 'james' : value;
 });
 console.log(json); // {"name":"james","color":"white","size":null,"birthDate":"2023-02-28T07:38:30.304Z"}
+console.log(jsonType); // string
 
 // 2. From JSON to Object
 // parse(JSON)
 json = JSON.stringify(rabbit);
 const obj = JSON.parse(json);
+const objType = typeof obj;
 
 console.log(obj); // {name: 'hcs1105', color: 'white', size: null, birthDate: '2023-02-28T07:45:00.345Z'}
+console.log(objType); // object
 
 rabbit.jump(); // can be jump!
 // obj.jump(); // Uncaught TypeError: obj.jump is not a function
 
-console.log(rabbit.birthDate.getDate()); // 28
-console.log(typeof obj.birthDate.getDate()); // Uncaught TypeError: obj.birthDate.getDate is not a function
+console.log(rabbit.birthDate.getDate()); // 26
+// console.log(obj.birthDate.getDate()); Uncaught TypeError: obj.birthDate.getDate is not a function
+
+// 파라미터 reviver를 이용하여 getDate() 메소드의 값이 표시되지 않는 현상 해결
+const obj1 = JSON.parse(json, (key, value) => {
+  console.log(`key : ${key}, value : ${value}`);
+  return key === 'birthDate' ? new Date() : value;
+});
+
+const objType1 = typeof obj1;
+console.log(objType1); // object
+
+console.log(rabbit.birthDate.getDate()); // 26
+console.log(obj1.birthDate.getDate()); // 26
+console.log(typeof obj1.birthDate); // object
+console.log(typeof obj1.birthDate.getDate()); // number
