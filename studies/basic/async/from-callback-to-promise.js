34c7ff4937bcb9448c686fa897d90d7c06cc2a3d
hcs1105 <hcs1105@gmail.com>
2023-03-30 21:01:02 +0900

    콜백 지옥을 promise로 해결

--- studies/basic/async/from-callback-to-promise.js
+++ studies/basic/async/from-callback-to-promise.js
@@ -2,24 +2,28 @@
 
 // callback hell example
 class UserStorage {
-  loginUser(id, password, onSuccess, onError) {
-    setTimeout(() => {
-      if(id === 'hcs1105' && password === '1111' || id === 'fedev1105' && password === '2222') {
-        onSuccess(id);
-      } else {
-        onError(new Error('Not found'));
-      }
-    }, 2000);
+  loginUser(id, password) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if(id === 'hcs1105' && password === '1111' || id === 'fedev1105' && password === '2222') {
+          resolve(id);
+        } else {
+          reject(new Error('Not found'));
+        }
+      }, 2000);
+    });
   }
 
-  getRoles(user, onSuccess, onError) {
-    setTimeout(() => {
-      if(user === 'hcs1105') {
-        onSuccess({ name : 'hcs1105', role : 'admin'});
-      } else {
-        onError(new Error('Not access'));
-      }
-    }, 1000);
+  getRoles(user) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if(user === 'hcs1105') {
+          resolve({ name : 'hcs1105', role : 'admin'});
+        } else {
+          reject(new Error('Not access'));
+        }
+      }, 1000);
+    });
   }
 }
 
@@ -27,21 +31,7 @@ const userStorage = new UserStorage();
 const id = prompt('Enter your id.');
 const password = prompt('Enter your password.');
 
-userStorage.loginUser(
-  id, 
-  password, 
-  user => {
-    userStorage.getRoles(
-      user, 
-      userWithRole => {
-        console.log(`Hello! ${userWithRole.name}, you have a ${userWithRole.role} role.`);
-      }, 
-      error => {
-        console.log(error);
-      }
-    );
-  }, 
-  error => {
-    console.log(error);
-  } 
-);
+userStorage.loginUser(id, password)
+  .then(user => userStorage.getRoles(user))
+  .then(user => console.log(`Hello! ${user.name}, you have a ${user.role} role.`))
+  .catch(error => console.log(error));
