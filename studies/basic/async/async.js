89ff991696498fc925739226cf078996fa319109
hcs1105 <hcs1105@gmail.com>
2023-03-31 17:01:15 +0900

    await 내용 추가

--- studies/basic/async/async.js
+++ studies/basic/async/async.js
@@ -24,3 +24,44 @@ async function fetchUser() {
 const user2 = fetchUser();
 user2.then(userName => console.log(userName)); // fedev1105
 console.log(user2); // Promise {<fulfilled>: 'fedev1105'} -> [[Prototype]]: Promise, [[PromiseState]]: "fulfilled", [[PromiseResult]]: "fedev1105"
+
+// 3. await
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function getApple() {
+  await delay(1000);
+  return 'Apple';
+}
+
+async function getBanana() {
+  await delay(1000);  
+  return 'Banana';
+}
+
+/*
+ * Promise로 구현한다면
+function getBanana() {
+  return delay(3000)
+  .then(() => 'banana');
+}
+ */
+
+async function pickFruits() {
+  const apple = await getApple();
+  const banana = await getBanana();
+
+  return `${apple} + ${banana}`;
+}
+
+/*
+ * Promise로 구현한다면
+function pickFruits() {
+  return getApple().then(apple => {
+    return getBanana().then(banana => `${apple} + ${banana}`);
+  });
+}
+ */
+
+pickFruits().then(fruit => console.log(fruit)); // Apple + Banana
