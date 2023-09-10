9af473df8351fe5ad0b7796bdd63f0bb4164a2ce
hcs1105 <hcs1105@gmail.com>
2023-09-10 21:08:32 +0900

    class로 간단한 2D 게임 만들기 진행 중

--- lecture/codingApple/2d-game.js
+++ lecture/codingApple/2d-game.js
@@ -1,5 +1,10 @@
 const canvas = document.querySelector('.canvas-1');
 const context = canvas.getContext('2d'); 
+const obstacleSet = [];
+
+let timer = 0;
+let jump = false;
+let jumpTimer = 0;
 
 canvas.width = window.innerWidth - 100;
 canvas.height = window.innerHeight - 100;
@@ -41,8 +46,7 @@ class Obstacle {
 	}
 }
 
-let timer = 0;
-const obstacleSet = [];
+
 
 function animationFrames() {
 	requestAnimationFrame(animationFrames);
@@ -56,13 +60,42 @@ function animationFrames() {
 		obstacleSet.push(obstacle_1);
 	}
 
-	obstacleSet.forEach(element => {
+	obstacleSet.forEach((element, i) => {
+		// X 좌표가 0 미만이면 필요 없는 장애물 제거
+		if(element.x <= 0) {
+			console.log('X 좌표가 0 미만이면 필요 없는 장애물 제거');
+			obstacleSet.splice(i, 1);
+		}
 		element.x--;
 		element.draw();
 	});
+
+	if(jump === true ) {
+		dinosaur.y--;
+		jumpTimer++;
+	} 
+
+	if(jump === false ) {
+		if(dinosaur.y < 200) {
+			dinosaur.y++;
+		}
+	}
+
+	if(jumpTimer > 100) {
+		jump = false;
+		jumpTimer = 0;
+	}
 	
-	// dinosaur.x++;
+	console.log(obstacleSet);
+
 	dinosaur.draw();
 }
 
 animationFrames();
+
+// 스페이스바를 누르면 공룡 접프하기
+document.addEventListener('keydown', (e) => {
+	if(e.code === 'Space') {
+		jump = true;
+	}
+});
